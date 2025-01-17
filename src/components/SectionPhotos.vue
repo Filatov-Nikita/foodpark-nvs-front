74a6f14d36b4378b2b9a7c259ed0a2128b8906cf
Nikita Antonov <nikita45454@gmail.com>
2025-01-17 12:30:13 +0300

    feat: photos- добавляет декоративные панели

--- src/components/SectionPhotos.vue
+++ src/components/SectionPhotos.vue
@@ -33,11 +33,24 @@
     width: 250px;
     height: 50%;
     z-index: -1;
+    animation: move-block1 10s ease-in-out infinite;
     @apply tw-bg-yellow;
 
     @include sm {
       top: -15px;
       width: 120px;
+    }
+  }
+
+  @keyframes move-block1 {
+    0% {
+      transform: translateY(0px);
+    }
+    50% {
+      transform: translateY(30%);
+    }
+    100% {
+      transform: translateY(0px);
     }
   }
 
@@ -48,6 +61,7 @@
     width: 70%;
     height: 175px;
     z-index: -1;
+    animation: move-block2 10s ease-in-out infinite;
     @apply tw-bg-yellow;
 
     @include sm {
@@ -56,17 +70,45 @@
     }
   }
 
+  @keyframes move-block2 {
+    0% {
+      transform: translateX(0px) translateY(0px);
+    }
+    50% {
+      transform: translateX(15%) translateY(10px);
+    }
+    100% {
+      transform: translateX(0px)  translateY(0px);
+    }
+  }
+
   .block3 {
+    --start-offset: 100px;
+    --top-gap: 20px;
     position: absolute;
-    top: 100px;
+    top: var(--start-offset);
     right: 0px;
     width: 175px;
     height: 60%;
     z-index: -1;
+    animation: move-block3 10s ease-in-out infinite;
     @apply tw-bg-yellow;
 
     @include sm {
-      top: 50px;
+      --start-offset: 50px;
+      --top-gap: 10px;
+    }
+  }
+
+  @keyframes move-block3 {
+    0% {
+      transform: translateY(10%);
+    }
+    50% {
+      transform: translateY(calc(var(--start-offset) * -1 - var(--top-gap)));
+    }
+    100% {
+      transform: translateY(10%);
     }
   }
 </style>
