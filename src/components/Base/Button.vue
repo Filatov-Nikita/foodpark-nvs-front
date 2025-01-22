adfc93369731a7f8176e96c18c2314eed84385c1
Nikita Antonov <nikita45454@gmail.com>
2025-01-23 00:04:49 +0300

    feat: Button - добавляет дизайн с рамкой

--- src/components/Base/Button.vue
+++ src/components/Base/Button.vue
@@ -1,6 +1,7 @@
 <template>
   <component
     class="button"
+    :class="`button--${design}`"
     :is="nativeLink ? 'a' : 'button'"
     :href="href"
     :disabled="disabled"
@@ -22,28 +23,47 @@
     disabled: {
       default: undefined,
       type: Boolean,
-    }
+    },
+    design: {
+      default: 'filled',
+      type: String,
+    },
   });
 </script>
 
 <style scoped lang="scss">
   .button {
     display: inline-block;
-    padding: 16px 30px;
     font-size: 16px;
     line-height: 1.25;
-    font-weight: 500;
     border-radius: 30px;
     text-align: center;
-    @apply tw-bg-primary tw-text-white tw-tracking-1 tw-uppercase;
+    @apply tw-tracking-1 tw-uppercase;
+
+    &--filled {
+      padding: 16px 30px;
+      font-weight: 500;
+      @apply tw-bg-primary tw-text-white;
+
+      &:hover {
+        background-color: #de1e54;
+      }
+    }
+
+    &--bordered {
+      padding: 15px 30px;
+      font-weight: 400;
+      border: 1px solid #E8D8B5;
+      @apply tw-text-gray;
+
+      &:hover {
+        border-color: #b9ac91;
+      }
+    }
 
     @include sm {
       padding: 12px 30px;
       font-size: 14px;
-    }
-
-    &:hover {
-      background-color: #de1e54;
     }
   }
 </style>
