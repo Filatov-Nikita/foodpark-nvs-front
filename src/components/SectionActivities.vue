a859e59088d3cde24372070f51a30928b7ac86ec
Nikita Antonov <nikita45454@gmail.com>
2025-04-18 13:59:17 +0500

    revert: отменяет желтые блоки на фото и мероприятиях

--- src/components/SectionActivities.vue
+++ src/components/SectionActivities.vue
@@ -1,22 +1,20 @@
 <template>
-  <section ref="activitiesRef" id="activities" class="activities">
+  <section id="activities" class="activities">
     <div class="wrapper">
       <h2 class="h2 h2--red tw-text-center">Мероприятия</h2>
       <TypesList class="types-list" :items="types" :activeType="activeType" @change:type="activeType = $event" />
       <ActivitiesList class="event-list" :items="elements" @changed:item="onChangeItem" />
       <DetailedModal v-if="activeActivity" v-model="showedActivity" :activityId="activeActivity" />
     </div>
-    <div class="activities-decor" ref="decorEl" :style="decorStyles"></div>
   </section>
 </template>
 
 <script setup>
-  import { computed, nextTick, ref, watch } from 'vue';
+  import { ref, watch } from 'vue';
   import TypesList from './Activities/TypesList.vue';
   import ActivitiesList from './Activities/List/index.vue';
   import DetailedModal from './Activities/DetailedModal.vue';
   import api from '@/repositories';
-  import useIntersect from '@/composables/useIntersect';
 
   const typesRes = await api.activities.types();
   const types = ref(typesRes);
@@ -38,66 +36,11 @@
     const res = await api.activities.elements(activeType.value);
     elements.value = res;
   });
-
-  const decorEl = ref(null);
-  const activitiesRef = ref(null);
-  const decorPos = ref({
-    top: 0,
-  });
-
-  watch(elements, () => {
-    if(elements.value.length === 0) {
-      decorPos.value.top = 0;
-    } else {
-      nextTick(() => calcDecorOffset());
-    }
-  });
-
-  const decorStyles = computed(() => {
-    return {
-      'transform': `translateY(${decorPos.value.top}px)`,
-    }
-  });
-
-  function calcDecorOffset() {
-    const swiper = document.querySelector('#activities .swiper');
-    if(!swiper) return;
-    const swiperWrap = swiper.querySelector('.swiper-wrapper');
-    const img = swiper.querySelector('.img-wrap');
-    const top = parseInt(getComputedStyle(decorEl.value).getPropertyValue('top'));
-    decorPos.value.top = swiperWrap.offsetTop + swiper.offsetTop + ((img.offsetHeight - decorEl.value.offsetHeight) / 2) - top;
-  }
-
-  useIntersect(
-    activitiesRef,
-    () => {
-      calcDecorOffset();
-    },
-    () => {
-      decorPos.value.top = 0;
-    },
-    { threshold: 0.7, once: false },
-  );
 </script>
 
 <style scoped lang="scss">
   .activities {
     position: relative;
-  }
-
-  .activities-decor {
-    position: absolute;
-    top: 30px;
-    left: 0;
-    width: 100%;
-    height: 210px;
-    z-index: -1;
-    transition: transform 800ms;
-    @apply tw-bg-yellow;
-
-    @include sm {
-      top: 15px;
-    }
   }
 
   .types-list {
--- src/components/SectionPhotos.vue
+++ src/components/SectionPhotos.vue
@@ -1,13 +1,10 @@
 <template>
   <section id="photos" class="photos">
-    <div class="wrapper tw-relative">
+    <div class="wrapper">
       <picture>
         <source media="(max-width: 720.9px)" :width="photoSm.width" :height="photoSm.height" :srcset="photoSm.url">
         <img class="tw-w-full" :width="photoXl.width" :height="photoXl.height" :src="photoXl.url" alt="Фото ресторанов" loading="lazy">
       </picture>
-      <div class="block1"></div>
-      <div class="block2"></div>
-      <div class="block3"></div>
     </div>
   </section>
 </template>
@@ -25,90 +22,4 @@
   });
 </script>
 
-<style scoped lang="scss">
-  .block1 {
-    position: absolute;
-    top: -30px;
-    left: 0px;
-    width: 250px;
-    height: 50%;
-    z-index: -1;
-    animation: move-block1 10s ease-in-out infinite;
-    @apply tw-bg-yellow;
-
-    @include sm {
-      top: -15px;
-      width: 120px;
-    }
-  }
-
-  @keyframes move-block1 {
-    0% {
-      transform: translateY(0px);
-    }
-    50% {
-      transform: translateY(30%);
-    }
-    100% {
-      transform: translateY(0px);
-    }
-  }
-
-  .block2 {
-    position: absolute;
-    bottom: -30px;
-    left: 100px;
-    width: 70%;
-    height: 175px;
-    z-index: -1;
-    animation: move-block2 10s ease-in-out infinite;
-    @apply tw-bg-yellow;
-
-    @include sm {
-      bottom: -15px;
-      left: 40px;
-    }
-  }
-
-  @keyframes move-block2 {
-    0% {
-      transform: translateX(0px) translateY(0px);
-    }
-    50% {
-      transform: translateX(15%) translateY(10px);
-    }
-    100% {
-      transform: translateX(0px)  translateY(0px);
-    }
-  }
-
-  .block3 {
-    --start-offset: 100px;
-    --top-gap: 20px;
-    position: absolute;
-    top: var(--start-offset);
-    right: 0px;
-    width: 175px;
-    height: 60%;
-    z-index: -1;
-    animation: move-block3 10s ease-in-out infinite;
-    @apply tw-bg-yellow;
-
-    @include sm {
-      --start-offset: 50px;
-      --top-gap: 10px;
-    }
-  }
-
-  @keyframes move-block3 {
-    0% {
-      transform: translateY(10%);
-    }
-    50% {
-      transform: translateY(calc(var(--start-offset) * -1 - var(--top-gap)));
-    }
-    100% {
-      transform: translateY(10%);
-    }
-  }
-</style>
+<style scoped lang="scss"></style>
