8e31a2c3a524838c8197208231ade778089a0ef3
Nikita Antonov <nikita45454@gmail.com>
2025-10-02 14:41:45 +0300

    feat: добавляет spinner при загрузке страницы

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './assets/scss/styles.scss';
 import BaseButton from '@/components/Base/Button.vue';
 import BaseBadge from '@/components/Base/Badge.vue';
 import BaseModal from '@/components/Base/Modal.vue';
+import BaseSpinner from '@/components/Base/Spinner.vue';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -14,6 +15,7 @@ const app = createApp(App);
 app.component('BaseButton', BaseButton);
 app.component('BaseBadge', BaseBadge);
 app.component('BaseModal', BaseModal);
+app.component('BaseSpinner', BaseSpinner);
 app.component('Swiper', Swiper);
 app.component('SwiperSlide', SwiperSlide);
 
