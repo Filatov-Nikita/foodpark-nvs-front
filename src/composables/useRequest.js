efcbf1d41e674fb2c6b7a5145fba1f77fff9623c
Nikita Antonov <nikita45454@gmail.com>
2025-10-06 23:34:03 +0300

    feat: добавляет уведомления об ошибках в useRequest

--- src/composables/useRequest.js
+++ src/composables/useRequest.js
@@ -1,45 +1,53 @@
+import { useNotification } from '@kyvg/vue3-notification';
 import { ref, watch } from 'vue';
 
-export default async function(fetchFn, opts) {
-  const defaultOptions = {
+export default async function(fetchFn, params = {}) {
+  params = Object.assign({}, {
     immediate: true,
-    watch: [],
-  };
+  }, params);
 
-  const options = Object.assign({}, defaultOptions, opts);
-
+  const loading = ref(false);
   const data = ref(null);
   const error = ref(null);
-  const pending = ref(false);
+
+  const { notify } = useNotification();
 
   async function send() {
     try {
-      pending.value = true;
+      loading.value = true;
       const res = await fetchFn();
       data.value = res;
+      if(params.onSuccess) params.onSuccess(res);
       return res;
     } catch(e) {
       data.value = null;
       error.value = e;
+      if(params.errorMessage) {
+        notify({
+          type: 'error',
+          text: params.errorMessage,
+        });
+      }
+      return null;
     } finally {
-      pending.value = false;
+      loading.value = false;
     }
   }
 
-  if(options.immediate) {
+  if(params.immediate) {
     await send();
   }
 
-  if(options.watch.length > 0) {
-    options.watch.forEach((w) => {
+  if(params.watch) {
+    params.watch.forEach(w => {
       watch(w, send);
     });
   }
 
   return {
+    loading,
     data,
     error,
-    pending,
     send,
   }
 }
